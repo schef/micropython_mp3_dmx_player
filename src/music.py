25661590ff2438dd283e0667bda6a66f8b1c93e9
schef <zvanstefan@gmail.com>
2025-03-29 08:09:32 +0100

    tested mp3

--- src/music.py
+++ src/music.py
@@ -10,27 +10,28 @@ class Song:
         self.volume = duration
 
 SONGS = {
-    1: Song(1, 100),
-    2: Song(2, 100),
+    1: Song(0, 100),
+    2: Song(1, 100),
     #3: Song(3, 100),
-    4: Song(3, 100),
-    5: Song(4, 100),
-    6: Song(5, 100),
+    4: Song(2, 100),
+    5: Song(3, 100),
+    6: Song(4, 100),
     #7: Song(7, 100),
-    8: Song(6, 100),
-    9: Song(7, 100),
-    10: Song(8, 100),
-    11: Song(9, 100),
-    12: Song(10, 100),
-    13: Song(11, 100),
-    14: Song(12, 100),
-    15: Song(13, 100),
-    16: Song(14, 100),
+    8: Song(5, 100),
+    9: Song(6, 100),
+    10: Song(7, 100),
+    11: Song(8, 100),
+    12: Song(9, 100),
+    13: Song(10, 100),
+    14: Song(11, 100),
+    15: Song(12, 100),
+    16: Song(13, 100),
     #17: Song(17, 100),
     #18: Song(18, 100),
-    19: Song(15, 100),
-    20: Song(16, 100),
-    21: Song(17, 100)
+    19: Song(14, 100),
+    20: Song(15, 100),
+    21: Song(16, 100),
+    90: Song(17, 100)
 }
 
 def play_by_index(index):
@@ -50,4 +51,4 @@ def stop():
 def init():
     global speaker
     speaker = KT403A(AUDIO_UART, volume=70)
-    play(21)
+    play(90)
